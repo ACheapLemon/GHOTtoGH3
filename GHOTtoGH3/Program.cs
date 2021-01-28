85f8bd33b3eab2a9574c89f88375732b904f66b0
acheaplemon <ipodhero7@gmail.com>
2021-01-29 02:19:14 +1030

    Small optimisation and comment fix

--- GHOTtoGH3/Program.cs
+++ GHOTtoGH3/Program.cs
@@ -33,32 +33,36 @@ namespace GHOTtoGH3 {
 
                     // ~~~Beatlines~~~ //
                     case "frets":
+                        // Create directory and file
                         directory = directory + "beatlines\\";
                         if (!Directory.Exists(directory))
                             Directory.CreateDirectory(directory);
-
                         outputFileName = directory + "beatlines.array.txt";
                         StreamWriter fretsFile = new StreamWriter(outputFileName);
-                        int dec = 0;
+                        int dec = 0, a = 1;
                         Boolean firstLine = true;
 
-                        for (int i = 0; (hexIn = infile.ReadByte()) != -1; i++)
+                        // Start reading byte characters from file
+                        while ((hexIn = infile.ReadByte()) != -1)
                         {
-                            // Chunk length of 4 bytes
-                            if (i % 4 == 0 && i != 0) {
+                            hex = string.Format("{0:X2}", hexIn) + hex;
+                            // Check if current hex length is 4 bytes
+                            if (a % 4 == 0 && a != 0) {
                                 dec = Convert.ToInt32(hex, 16);
 
+                                // If first beatline is not '0', print 0 first
                                 if (firstLine == true && dec != 0) {
                                     fretsFile.WriteLine("0");
                                 }
                                 firstLine = false;
 
+                                // Print beatline
                                 Console.WriteLine(dec);
                                 fretsFile.WriteLine(dec);
                                 hex = "";
                                 dec = 0;
                             }
-                            hex = string.Format("{0:X2}", hexIn) + hex;
+                            a++;
                         }
                         fretsFile.Close();
                         break;
@@ -69,7 +73,7 @@ namespace GHOTtoGH3 {
                     case "gems":
                         string fullFileType = inputFileName.Substring(inputFileName.IndexOf('_') + 1, inputFileName.IndexOf('.') - inputFileName.IndexOf('_') - 1);
 
-                        // Rename note types
+                        // Rename note types to match EOF import
                         switch (fullFileType) {
                             case "gems_easy":
                                 fullFileType = "lead_Easy";
@@ -101,42 +105,30 @@ namespace GHOTtoGH3 {
                                 break;
                         }
 
+                        // Create directory and file
                         directory = directory + fullFileType;
                         if (!Directory.Exists(directory))
                             Directory.CreateDirectory(directory);
-
                         outputFileName = directory + "\\" + fullFileType + ".array.txt";
                         StreamWriter gemsFile = new StreamWriter(outputFileName);
-                        gemsFile.WriteLine("HOPO");
-                        int posDec = 0;
-                        int typeDec = 0;
-                        int infoDec = 0;
-                        int lenDec = 0;
 
-                        Boolean prevSP = false;
-                        Boolean hopoFlag = false;
-                        int startPos = 0;
-                        int startType = 0;
-                        int spLen = 0;
-                        int prevSPms = 0;
-                        int a = 0;
+                        // Initialise note file contents and variables
+                        gemsFile.WriteLine("HOPO");
+                        int posDec = 0, typeDec = 0, infoDec = 0, lenDec = 0;
+                        int startPos = 0, startType = 0, spLen = 0, prevSPms = 0, b = 1;
+                        Boolean prevSP = false, hopoFlag = false;
 
                         while ((hexIn = infile.ReadByte()) != -1)
                         {
-                            // Chunk length of 8 bytes 0 0 00 0000
-                            if (a % 8 == 0 && a != 0) {
-                                string posHex = hex.Substring(8, 8);
-                                string typeHex = hex.Substring(0, 2);
-                                string infoHex = hex.Substring(2, 2);
-                                string lenHex = hex.Substring(4, 4);
+                            hex = string.Format("{0:X2}", hexIn) + hex;
+                            // Check if current hex length is 8 bytes (0|0|00|0000)
+                            if (b % 8 == 0 && b != 0) {
 
-                                posDec = Convert.ToInt32(posHex, 16);
-                                typeDec = Convert.ToInt32(typeHex, 16);
-                                infoDec = Convert.ToInt32(infoHex, 16);
-                                lenDec = Convert.ToInt32(lenHex, 16);
-                                
-                                if (lenDec == 0)
-                                    lenDec = 1;
+                                // Get hex for each part of chunk and convert to decimal
+                                typeDec = Convert.ToInt32(hex.Substring(0, 2), 16);
+                                infoDec = Convert.ToInt32(hex.Substring(2, 2), 16);
+                                lenDec = Convert.ToInt32(hex.Substring(4, 4), 16);
+                                posDec = Convert.ToInt32(hex.Substring(8, 8), 16);
 
                                 // Find HOPO info
                                 int hopoInfoDec = infoDec;
@@ -173,11 +165,10 @@ namespace GHOTtoGH3 {
                                     prevSP = false;
                                 }
 
-                                Console.WriteLine(hex);
-                                Console.WriteLine(posDec);
-                                Console.WriteLine(typeDec);
-                                Console.WriteLine(lenDec);
+                                // Print note information
                                 gemsFile.WriteLine(posDec);
+                                if (lenDec == 0)
+                                    lenDec = 1;
                                 gemsFile.WriteLine(lenDec);
                                 if (hopoFlag == true) {
                                     gemsFile.WriteLine("H" + typeDec);
@@ -187,90 +178,47 @@ namespace GHOTtoGH3 {
                                 {
                                     gemsFile.WriteLine(typeDec);
                                 }
+                                
                                 hex = "";
                                 dec = 0;
                             }
-                            hex = string.Format("{0:X2}", hexIn) + hex;
-                            a++;
-                        }
-                        // Chunk length of 8 bytes 0 0 00 0000
-                        if (a % 8 == 0 && a != 0)
-                        {
-                            string posHex = hex.Substring(8, 8);
-                            string typeHex = hex.Substring(0, 2);
-                            string infoHex = hex.Substring(2, 2);
-                            string lenHex = hex.Substring(4, 4);
-
-                            posDec = Convert.ToInt32(posHex, 16);
-                            typeDec = Convert.ToInt32(typeHex, 16);
-                            infoDec = Convert.ToInt32(infoHex, 16);
-                            lenDec = Convert.ToInt32(lenHex, 16);
-
-                            if (lenDec == 0)
-                                lenDec = 1;
-
-                            // Find HOPO info
-                            int hopoInfoDec = infoDec;
-                            if (infoDec / 128 >= 1)
-                            {
-                                hopoInfoDec = infoDec - 128;
-                            }
-                            if (hopoInfoDec / 64 >= 1)
-                            {
-                                hopoFlag = true;
-                            }
-
-                            // Find SP info
-                            if (infoDec % 2 == 1 && infoDec != 0)
-                            {
-                                prevSPms = posDec;
-                                if (prevSP == false)
-                                {
-                                    prevSP = true;
-                                    startPos = posDec;
-                                    startType = typeDec;
-                                }
-                            }
-                            else
-                            {
-                                // If end of SP phase
-                                if (prevSP == true)
-                                {
-                                    spLen = prevSPms - startPos + 1;
-
-                                    string spDirectory = directory + "_SP\\";
-                                    if (!Directory.Exists(spDirectory))
-                                        Directory.CreateDirectory(spDirectory);
-                                    StreamWriter spFile = new StreamWriter(spDirectory + startPos + ".array.txt");
-                                    spFile.WriteLine(startPos);
-                                    spFile.WriteLine(spLen);
-                                    spFile.WriteLine("5");
-                                    spFile.Close();
-                                }
-                                prevSP = false;
-                            }
-
-                            Console.WriteLine(hex);
-                            Console.WriteLine(posDec);
-                            Console.WriteLine(typeDec);
-                            Console.WriteLine(lenDec);
-                            gemsFile.WriteLine(posDec);
-                            gemsFile.WriteLine(lenDec);
-                            if (hopoFlag == true)
-                            {
-                                gemsFile.WriteLine("H" + typeDec);
-                                hopoFlag = false;
-                            }
-                            else
-                            {
-                                gemsFile.WriteLine(typeDec);
-                            }
-                            hex = "";
-                            dec = 0;
+                            b++;
                         }
                         gemsFile.Close();
                         break;
                     // ~~~~~~~~~~~ //
+
+
+                    // ~~~Sections (English)~~~ //
+                    case "sections_English":
+                        // Create directory and file
+                        directory = directory + "sections\\";
+                        if (!Directory.Exists(directory))
+                            Directory.CreateDirectory(directory);
+                        outputFileName = directory + "sections.array.txt";
+                        StreamWriter sectionsFile = new StreamWriter(outputFileName);
+
+                        string stringHex = "";
+                        int c = 1;
+
+
+                        while ((hexIn = infile.ReadByte()) != -1)
+                        {
+                            hex = string.Format("{0:X2}", hexIn) + hex;
+                            stringHex = stringHex + string.Format("{0:X2}", hexIn);
+
+                            // Check if current hex length is 20 bytes
+                            if (c % 20 == 0 && c != 0)
+                            {
+                                PrintSection(hex, stringHex, sectionsFile);
+                                hex = "";
+                                stringHex = "";
+                            }
+                            c++;
+                        }
+                        sectionsFile.Close();
+                        break;
+                    // ~~~~~~~~~~~~~~~ //
 
 
                     // ~~~Timesig~~~ //
@@ -281,42 +229,10 @@ namespace GHOTtoGH3 {
 
                         outputFileName = directory + "0.array.txt";
                         StreamWriter sigFile = new StreamWriter(outputFileName);
-                        sigFile.WriteLine("0");
-                        sigFile.WriteLine("4");
-                        sigFile.Write("4");
+                        sigFile.Write("0\n4\n4");
                         sigFile.Close();
                         break;
                     // ~~~~~~~~~~~~~ //
-
-
-                    // ~~~Sections (English)~~~ //
-                    case "sections_English":
-                        directory = directory + "sections\\";
-                        if (!Directory.Exists(directory))
-                            Directory.CreateDirectory(directory);
-
-                        outputFileName = directory + "sections.array.txt";
-                        StreamWriter sectionsFile = new StreamWriter(outputFileName);
-                        string stringHex = "";
-
-                        for (int i = 0; (hexIn = infile.ReadByte()) != -1; i++)
-                        {
-                            // Chunk length of 20 bytes
-                            if (i % 20 == 0 && i != 0)
-                            {
-                                PrintSection(hex, stringHex, sectionsFile);
-                                hex = "";
-                                stringHex = "";
-                            }
-                            hex = string.Format("{0:X2}", hexIn) + hex;
-                            stringHex = stringHex + string.Format("{0:X2}", hexIn);
-                        }
-                        
-                        PrintSection(hex, stringHex, sectionsFile);
-
-                        sectionsFile.Close();
-                        break;
-                    // ~~~~~~~~~~~~~~~ //
 
 
                     default:
@@ -341,11 +257,9 @@ namespace GHOTtoGH3 {
                     ascii += character;
 
                 }
-
                 return ascii;
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
-
             return string.Empty;
         }
 
